2fd33f1733ff269985c0c2e13d9fc1ff5856ec2f
Onii-chan <failer007@live.dk>
2022-11-24 17:01:41 +0100

    Potential Angular version conflicts fix
    I upgraded my own CLI to 14.2.10 and the entire project was made from scratch in respect for the latest changes.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,14 @@
 export const environment = {
   production: true,
-  api: "",
+  useHttps: true,
+  domain: "localhost",
+  port: 5000,
+
+  getApiAddress() {
+    let useHttps = environment.useHttps;
+    let domain = environment.domain;
+    let port = environment.port;
+
+    return (useHttps ? "https://" : "http://") + domain + ":" + port + "/";
+  }
 };
